7fd6e572880308775f119c70197c85e05e87121a
Christoph Dorn <christoph@christophdorn.com>
2015-09-15 00:26:29 -0700

    wip

--- for/knexjs/0-server.ccjson.js
+++ for/knexjs/0-server.ccjson.js
@@ -1,7 +1,94 @@
 
-var Entity = exports.Entity = function () {
+exports.forLib = function (LIB) {
+    var ccjson = this;
 
-console.log("new knexjs entity");
+    const Q = require("q");
+    const KNEX = require("knex");
 
+    return LIB.Promise.resolve({
+        forConfig: function (defaultConfig) {
 
+            var Entity = function (instanceConfig) {
+                var self = this;
+
+                self.AspectInstance = function (aspectConfig) {
+
+                    var config = {};
+                    LIB._.merge(config, defaultConfig);
+                    LIB._.merge(config, instanceConfig);
+                    LIB._.merge(config, aspectConfig);
+                    config = ccjson.attachDetachedFunctions(config);
+
+console.log("config", config);
+
+                	// @see http://knexjs.org/#Installation-node
+                	var knex = KNEX(config.knex);
+
+                    var context = {
+                        adapter: {
+                            knex: function (tableName, query) {
+                        		if (typeof query === "undefined" && typeof tableName === "function") {
+                        			query = tableName;
+                        			tableName = null;
+                        		}
+                    			var table = ((tableName) ? knex(tableName) : knex);
+                    			return query(table).then(function (resp) {
+                    
+console.log("RESPONSE:", resp);
+                    
+                    				return resp;
+                    			}).catch(function (err) {
+                        			console.error("DB Error:", err.stack);
+                        			throw err;
+                        		});
+                        	},
+                        	qknex: function (tableName, query) {
+                        		if (typeof query === "undefined" && typeof tableName === "function") {
+                        			query = tableName;
+                        			tableName = null;
+                        		}
+                        		return Q.fcall(function () {
+                        			var table = ((tableName) ? knex(tableName) : knex);
+                        			return query(table).then(function (resp) {
+                        
+console.log("RESPONSE:", resp);
+                        
+                        				return resp;
+                        			});
+                        		}).catch(function (err) {
+                        			console.error(err.stack);
+                        			throw err;
+                        		});
+                        	}
+                        }
+                    };
+
+                    return LIB.Promise.resolve({
+                        app: function () {
+                            return LIB.Promise.resolve(
+                                ccjson.makeDetachedFunction(
+                                    function (req, res, next) {
+                                        if (
+                                            config.request &&
+                                            config.request.contextAlias
+                                        ) {
+                                            if (!req.context) {
+                                                req.context = {};
+                                            }
+                                            req.context[config.request.contextAlias] = context;
+                                        }
+                                        return next();
+                                    }
+                                )
+                            );
+                        }
+                    });
+                }
+
+            }
+            Entity.prototype.config = defaultConfig;
+
+            return Entity;
+        }
+    });
 }
