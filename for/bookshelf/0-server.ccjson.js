0fcaa6c8fcc6b68d1b1aec9e95df4350313071ec
Christoph Dorn <christoph@christophdorn.com>
2015-09-24 01:15:26 -0700

    wip

--- for/bookshelf/0-server.ccjson.js
+++ for/bookshelf/0-server.ccjson.js
@@ -11,139 +11,102 @@ exports.forLib = function (LIB) {
             var Entity = function (instanceConfig) {
                 var self = this;
 
-                self.AspectInstance = function (aspectConfig) {
-
-                    var config = {};
-                    LIB._.merge(config, defaultConfig);
-                    LIB._.merge(config, instanceConfig);
-                    LIB._.merge(config, aspectConfig);
-                    config = ccjson.attachDetachedFunctions(config);
+                var config = {};
+                LIB._.merge(config, defaultConfig);
+                LIB._.merge(config, instanceConfig);
+                config = ccjson.attachDetachedFunctions(config);
 
 //console.log("config", config);
 
-                    var knex = config.knex();
+                var knex = config.knex();
 
-                    var context = {
-                        adapter: {},
-                        models: {},
-                        helpers: {
-                          collateRecords: function (ownCollection, records) {
-                            var collections = {};
-                            var collectionsById = {};
-                            function addRecord(collection, record) {
-                              if (!collections[collection]) {
-                                collections[collection] = [];
-                                collectionsById[collection] = {};
-                              }
-                              if (collectionsById[collection][record.id]) {
-                                  return;
-                              }
-                              collectionsById[collection][record.id] = record;
-                              collections[collection].push(record);
-                            }
-                            records.forEach(function (record) {
-                              for (var name in record) {
-                                if (
-                                    record[name] &&
-                                    typeof record[name] !== "string" &&
-                                    typeof record[name] === "object" &&
-                                    typeof record[name].id !== "undefined" &&
-                                    context.models[ownCollection]["@fields"][name] &&
-                                    context.models[ownCollection]["@fields"][name].linksToOne
-                                ) {
-                                  addRecord(context.models[ownCollection]["@fields"][name].linksToOne, record[name]);
-                                  record[name] = record[name].id;
-                                }
-                              }
-                              addRecord(ownCollection, record);
-                            });
-                            return collections;
-                          }                            
+                var context = config.context();
+
+                var api = {
+                    adapter: {},
+                    models: {},
+                    helpers: {
+                      collateRecords: function (ownCollection, records) {
+                        var collections = {};
+                        var collectionsById = {};
+                        function addRecord(collection, record) {
+                          if (!collections[collection]) {
+                            collections[collection] = [];
+                            collectionsById[collection] = {};
+                          }
+                          if (collectionsById[collection][record.id]) {
+                              return;
+                          }
+                          collectionsById[collection][record.id] = record;
+                          collections[collection].push(record);
                         }
-                    };
-
-                    // TODO: See why we get instance requests without knex set
-                    if (knex) {
-                        context.adapter.bookshelf = BOOKSHELF(knex);
-                    }
-
-                    var contexts = new CONTEXTS.adapters.context.server.Context({
-                        "data": {
-                            "config": {},
-                            "adapters": {
-                                "mapper": "ccjson.record.mapper"
+                        records.forEach(function (record) {
+                          for (var name in record) {
+                            if (
+                                record[name] &&
+                                typeof record[name] !== "string" &&
+                                typeof record[name] === "object" &&
+                                typeof record[name].id !== "undefined" &&
+                                api.models[ownCollection]["@fields"][name] &&
+                                api.models[ownCollection]["@fields"][name].linksToOne
+                            ) {
+                              addRecord(api.models[ownCollection]["@fields"][name].linksToOne, record[name]);
+                              record[name] = record[name].id;
                             }
-                        },
-                        "time": {
-                            "adapters": {
-                                "moment": "moment"
-                            }
-                        }
-                    });
-
-                    function buildModels () {
-                        if (
-                            !config.collectionsPath ||
-                            !context.adapter ||
-                            !context.adapter.bookshelf
-                        ) {
-                            return LIB.Promise.resolve();
-                        }
-                        return contexts.adapters.data.mapper.loadCollectionModelsForPath(config.collectionsPath).then(function (models) {
-
-                            return LIB.Promise.all(Object.keys(models).map(function (modelName) {
-
-                                var foreignFields = {};
-                                Object.keys(models[modelName].Record["@fields"]).forEach(function (fieldName) {
-                                    var field = models[modelName].Record["@fields"][fieldName];
-
-                                    if (field.linksToOne) {
-                                        foreignFields[fieldName] = function () {
-                                            return this.belongsTo(
-                                                context.models[field.linksToOne],
-                                                fieldName
-                                            );
-                                        }
-                                    }
-                                });
-
-                                context.models[modelName] = context.adapter.bookshelf.Model.extend(
-                                    LIB._.assign(foreignFields, {
-                                        tableName: modelName
-                                    })
-                                );
-                                context.models[modelName]["@fields"] = models[modelName].Record["@fields"]
-                            }));
-
+                          }
+                          addRecord(ownCollection, record);
                         });
+                        return collections;
+                      }                            
                     }
+                };
 
-
-                    return buildModels().then(function () {
-
-                        return LIB.Promise.resolve({
-                            app: function () {
-                                return LIB.Promise.resolve(
-                                    ccjson.makeDetachedFunction(
-                                        function (req, res, next) {
-                                            if (
-                                                config.request &&
-                                                config.request.contextAlias
-                                            ) {
-                                                if (!req.context) {
-                                                    req.context = {};
-                                                }
-                                                req.context[config.request.contextAlias] = context;
-                                            }
-                                            return next();
-                                        }
-                                    )
-                                );
-                            }
-                        });
-                    });
+                // TODO: See why we get instance requests without knex set
+                if (knex) {
+                    api.adapter.bookshelf = BOOKSHELF(knex);
                 }
 
+                context.setAdapterAPI(api);
+                
+
+                if (
+                    !config.collectionsPath ||
+                    !api.adapter ||
+                    !api.adapter.bookshelf
+                ) {
+                    return self;
+                }
+
+                return context.getAdapterAPI("data.knexjs.mapper").then(function (mapperApi) {
+                    var models = mapperApi.models;
+
+                    return LIB.Promise.all(Object.keys(models).map(function (modelName) {
+
+                        var foreignFields = {};
+                        Object.keys(models[modelName].Record["@fields"]).forEach(function (fieldName) {
+                            var field = models[modelName].Record["@fields"][fieldName];
+
+                            if (field.linksToOne) {
+                                foreignFields[fieldName] = function () {
+                                    return this.belongsTo(
+                                        api.models[field.linksToOne],
+                                        fieldName
+                                    );
+                                }
+                            }
+                        });
+
+                        api.models[modelName] = api.adapter.bookshelf.Model.extend(
+                            LIB._.assign(foreignFields, {
+                                tableName: modelName
+                            })
+                        );
+                        api.models[modelName]["@fields"] = models[modelName].Record["@fields"]
+                    }));
+
+                }).then(function () {
+                    return self;
+                });
             }
             Entity.prototype.config = defaultConfig;
 
--- for/knexjs/0-server.ccjson.js
+++ for/knexjs/0-server.ccjson.js
@@ -14,85 +14,67 @@ exports.forLib = function (LIB) {
                 var config = {};
                 LIB._.merge(config, defaultConfig);
                 LIB._.merge(config, instanceConfig);
+                config = ccjson.attachDetachedFunctions(config);
 
-                self.AspectInstance = function (aspectConfig) {
+                var context = config.context();
 
-                    var config = {};
-                    LIB._.merge(config, defaultConfig);
-                    LIB._.merge(config, instanceConfig);
-                    LIB._.merge(config, aspectConfig);
-                    config = ccjson.attachDetachedFunctions(config);
+                var api = {};
 
-                    var context = {};
+                // TODO: See why we get instance requests without sufficnet config set.
+                //       Some calls are ok, but maybe too many? This should probably move up
+                //       one layer to the entity instance.
+                if (
+                    config.knex &&
+                    config.knex.connection &&
+                    config.knex.connection.host
+                ) {
+                	// @see http://knexjs.org/#Installation-node
+                	var knex = KNEX(config.knex);
 
-                    // TODO: See why we get instance requests without sufficnet config set.
-                    //       Some calls are ok, but maybe too many? This should probably move up
-                    //       one layer to the entity instance.
-                    if (
-                        config.knex &&
-                        config.knex.connection &&
-                        config.knex.connection.host
-                    ) {
-                    	// @see http://knexjs.org/#Installation-node
-                    	var knex = KNEX(config.knex);
-    
-                        context.adapter = {
-                            knex: function (tableName, query) {
-                        		if (typeof query === "undefined" && typeof tableName === "function") {
-                        			query = tableName;
-                        			tableName = null;
-                        		}
+                    api.adapter = {
+                        knex: function (tableName, query) {
+                    		if (typeof query === "undefined" && typeof tableName === "function") {
+                    			query = tableName;
+                    			tableName = null;
+                    		}
+                			var table = ((tableName) ? knex(tableName) : knex);
+                			return query(table).then(function (resp) {
+                
+//console.log("RESPONSE:", resp);
+                
+                				return resp;
+                			}).catch(function (err) {
+                    			console.error("DB Error:", err.stack);
+                    			throw err;
+                    		});
+                    	},
+                    	qknex: function (tableName, query) {
+                    		if (typeof query === "undefined" && typeof tableName === "function") {
+                    			query = tableName;
+                    			tableName = null;
+                    		}
+                    		return Q.fcall(function () {
                     			var table = ((tableName) ? knex(tableName) : knex);
                     			return query(table).then(function (resp) {
                     
 //console.log("RESPONSE:", resp);
                     
                     				return resp;
-                    			}).catch(function (err) {
-                        			console.error("DB Error:", err.stack);
-                        			throw err;
-                        		});
-                        	},
-                        	qknex: function (tableName, query) {
-                        		if (typeof query === "undefined" && typeof tableName === "function") {
-                        			query = tableName;
-                        			tableName = null;
-                        		}
-                        		return Q.fcall(function () {
-                        			var table = ((tableName) ? knex(tableName) : knex);
-                        			return query(table).then(function (resp) {
-                        
-//console.log("RESPONSE:", resp);
-                        
-                        				return resp;
-                        			});
-                        		}).catch(function (err) {
-                        			console.error(err.stack);
-                        			throw err;
-                        		});
-                        	}
-                        };
-                    }
+                    			});
+                    		}).catch(function (err) {
+                    			console.error(err.stack);
+                    			throw err;
+                    		});
+                    	}
+                    };
+                }
+                
+                context.setAdapterAPI(api);
+
+
+                self.AspectInstance = function (aspectConfig) {
 
                     return LIB.Promise.resolve({
-                        app: function () {
-                            return LIB.Promise.resolve(
-                                ccjson.makeDetachedFunction(
-                                    function (req, res, next) {
-                                        if (
-                                            config.request &&
-                                            config.request.contextAlias
-                                        ) {
-                                            if (!req.context) {
-                                                req.context = {};
-                                            }
-                                            req.context[config.request.contextAlias] = context;
-                                        }
-                                        return next();
-                                    }
-                                )
-                            );
-                        },
                         connection: function () {
                             return LIB.Promise.resolve(
                                 ccjson.makeDetachedFunction(
